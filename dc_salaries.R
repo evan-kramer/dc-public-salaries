a9930e0b036c72acd3dc43d1e06f177b2b19a501
Kramer <evan.kramer@dc.gov>
2020-04-06 09:49:17 -0400

    Began text parsing

--- dc_salaries.R
+++ dc_salaries.R
@@ -9,7 +9,8 @@ library(haven)
 library(httr)
 library(rvest)
 library(pdftools)
-setwd("C:/Users/evan.kramer/Documents/dc_public_salaries")
+library(readxl)
+setwd("U:/dc_public_salaries")
 
 # Scrape from webpage?
 # Extract file urls
@@ -18,118 +19,122 @@ salary_files = tibble(
   link = as.character(html_nodes(url, "a")),
   link_start = str_locate(link, "https"),
   link_end = str_locate(link, ".pdf") + 3,
-  file_url = NA
+  # file_date_start = str_locate(link, '">Public Body Employee Information as of'),
+  file_date_start = str_locate(link, '">'),
+  file_url = NA,
+  file_name = NA,
+  file_date = NA
 ) %>% 
   filter(str_detect(str_to_lower(link), "as of")) 
 for(r in 1:nrow(salary_files)) {
   salary_files$file_url[r] = str_sub(salary_files$link[r], 
                                      salary_files$link_start[r],
                                      salary_files$link_end[r])
+  salary_files$file_name[r] = str_replace(
+    salary_files$file_url[r], 
+    "https://dchr.dc.gov/sites/default/files/dc/sites/dchr/publication/attachments/",
+    ""
+  )
+  salary_files$file_date[r] = str_sub(salary_files$link[r],
+                                      salary_files$file_date_start,
+                                      str_length(salary_files$link[r]) - 3)
 }
 
-# Download all PDFs -- Is there a way to read tables directly from URLs? 
-walk2(
-  .x = salary_files$file_url, 
-  .y = salary_files$file_url,
-  .f = download.filemode = "wb"
-)
-
-
-
-download.file(
-  url = salary_files$file_url[1], 
-  destfile = str_c(getwd(), "/", str_sub(salary_files$file_url[1], -15, -1))
-)
-
-
-
-# Get data from file urls
-for(f in salary_files$file_url) {
-  
+# Scrape leadership from web
+lt = read_html("https://osse.dc.gov/page/meet-leadership-team")
+lt_names = tibble(
+  link = as.character(html_nodes(lt, "a")),
+  lt_name_start = NA,
+  lt_name_end = NA,
+  lt_name = NA
+) %>% 
+  filter(str_detect(link, "email-protection"))
+for(r in 1:nrow(lt_names)) {
+  lt_names$lt_name_start[r] = str_locate(lt_names$link[r], ">")[2] + 1
+  lt_names$lt_name_end[r] = str_locate(lt_names$link[r], "</a>")[1] - 1
+  # lt_names$lt_name = str_sub(lt_names$link[r], lt_names$lt_name_start[r], lt_names$lt_name_end[r])
 }
+lt_names = mutate(lt_names, lt_name = str_sub(link, lt_name_start, lt_name_end)) %>% 
+  filter(!str_detect(lt_name, ">")) %>% 
+  transmute(full_name = lt_name) %>% 
+  separate(full_name, sep = " ", into = c("first_name", "last_name", "overflow"), remove = F) %>% 
+  transmute(
+    full_name,
+    first_name = ifelse(is.na(overflow), first_name, str_c(first_name, " ", last_name)),
+    last_name = ifelse(is.na(overflow), last_name, overflow)
+  )
 
-
-# Load file from internet
-dc = readxl::read_excel(
-  path = "C:/Users/CA19130/Downloads/public_body_employee_information_123118.xlsx",
-  # path = "C:/Users/CA19130/Downloads/public_body_employee_information_03312019.xlsx",
-  # path = "C:/Users/CA19130/Downloads/public_body_employee_information_06302019.xlsx",
-  col_names = F,
-  skip = 1
-)
-  
-
-
-
-# Check to see which variables contain all missing data
-y = c()
-for(v in 1:ncol(dc)) {
-  w = dc[, str_c("...", v)]
-  x = sum(!is.na(w))
-  if(x == 0) {
-    print(str_c("...", v)); print(x)
-    y = c(y, str_c("...", v))
+# Download files from internet
+for(f in 1:nrow(salary_files)) {
+  # Check if the file exists then download
+  if(!file.exists(str_c(getwd(), "/Raw Salary Files/", salary_files$file_name[f]))) {
+    download.file(
+      url = salary_files$file_url[f],
+      mode = "wb",
+      destfile = str_c(
+        getwd(), "/Raw Salary Files/", salary_files$file_name[f]
+      ),
+      quiet = T
+    )
   }
 }
-# Remove variables with all missing data
-for(v in y) {
-  dc[, v] = NULL
-}
-rm(y, v, w, x)
+rm(f)
 
-# How many nonmissing variables are there? Can we select only if character or datetime?
-dim(dc)
-
-# Create new variables
-dc = transmute(
-  dc, 
-  agency_name = `...1`, 
-  last_name = str_c(ifelse(is.na(`...3`), "", `...3`), 
-                    ifelse(is.na(`...5`), "", `...5`), 
-                    ifelse(is.na(`...6`), "", `...6`)),
-  first_name = str_c(ifelse(is.na(`...10`), "", `...10`),
-                     ifelse(is.na(`...13`), "", `...13`)),
-  type_appt = str_c(ifelse(is.na(`...14`), "", `...14`),
-                    ifelse(is.na(`...19`), "", `...19`)),
-  position_title = str_c(ifelse(is.na(`...20`), "", `...20`),
-                         ifelse(is.na(`...21`), "", `...21`)),
-  grade = str_c(ifelse(is.na(`...23`), "", `...23`),
-                ifelse(is.na(`...25`), "", `...25`)),
-  compensation = as.numeric(str_c(ifelse(is.na(`...26`), "", `...26`),
-                                  ifelse(is.na(`...28`), "", `...28`))),
-  hire_date = `...29`
-) 
-
-# Leadership team salaries
-first = c(
-  "lida", "darrell", "gretchen", "thomas", "sarah jane", "shavonne",
-  "elizabeth", "sarah", "sara", "antoinette", "elisabeth", "heidi",
-  "pete", "shana", "william", "hanseul", "don", "jason"
+# Extract information using `pdf_text`
+temp = pdf_text(
+  pdf = str_c(getwd(), "/Raw Salary Files/", list.files(str_c(getwd(), "/Raw Salary Files/"))[1])
 )
-last = c(
-  "alikhani", "ashton", "brumley", "fontenot", "forman", "gibson", 
-  "groginsky", "martin", "meyers", "mitchell", "morse", "schumacher",
-  "siu", "young", "henderson", "kang", "davis", "kim"
-)
-lteam = tibble()
-for(n in 1:length(first)) {
-  lteam = filter(dc, str_to_upper(last_name) == str_to_upper(last[n]) & 
-                  str_to_upper(first_name) == str_to_upper(first[n])) %>% 
-    bind_rows(lteam, .) %>% 
-    arrange(last_name)
-}
 
-# 130000, 175286, 176794, 173000, 157380, 161160, 170000, 126072, 180544,
-# 160766, 168000, 145438, 172597
-
-# What is the typical salary for directors
-filter(
-  dc, 
-  str_to_lower(agency_name) == "ofc. of state superintendent",
-  str_detect(str_to_lower(position_title), "director")
+test = tibble(
+  text = unlist(str_split(temp[1], "\r\n"))
 ) %>% 
-  summarize_at(
-    vars(compensation),
-    c("mean", "min", "max", "sd"),
-    na.rm = T
+  separate(text, into = str_c("v_", 1:100), sep = " ") %>% 
+  mutate_all("str_trim") %>% 
+  mutate_all("str_to_upper") %>% 
+  mutate_at(
+    vars(starts_with("v_")),
+    funs(ifelse(. == "", NA_character_, .))
+  ) %>% 
+  unite(col = "text2", starts_with("v_"), na.rm = T, sep = " ") %>% 
+  mutate(
+    appt_end = NA,
+    appt = NA, 
+    last_name = NA,
+    first_name = NA,
+    title = NA,
+    salary = NA,
+    hire_date = NA,
+    next_space = NA
   )
+
+test$text2[r]
+test["appt"] %>% unlist() %>% .[3]
+
+for(r in 1:nrow(test)) {
+  # Appointment
+  test$appt_end[r] = min(str_locate(test$text2[r], "APP"), na.rm = T)
+  test$appt[r] = str_sub(test$text2[r], 1, test$appt_end[r] + 3)
+  test$text2[r] = str_replace(
+    test$text2[r],
+    unlist(test["appt"])[r],
+    ""
+  ) %>% 
+    str_trim()
+    
+  # Loop through the rest
+  for(v in c("last_name", "first_name", "title", "salary", "hire_date")) {
+    # test$text2[r] = str_trim(str_replace_all(test$text2[r], test[, v][r,], ""))
+    test$next_space[r] = min(str_locate(str_trim(test$text2[r]), " "), na.rm = T)
+    test[, v][r,] = str_sub(test$text2[r], 1, test$next_space[r])
+    test$text2[r] = str_replace(
+      test$text2[r],
+      unlist(test[v])[r],
+      ""
+    ) %>% 
+      str_trim()
+  }
+}
+
+# We'll have to get creative around using the numbers from salaries/hire dates to pull 
+# titles of different lengths apart
+# And what about adding dates? 
