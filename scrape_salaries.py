18524c54849c4d3e91d7a9e29ede246371d4c4b2
Evan Kramer <kramer.evan@gmail.com>
2021-01-27 15:35:21 -0500

    Updated dcode for batch downloading and converting

--- scrape_salaries.py
+++ scrape_salaries.py
@@ -6,15 +6,15 @@ Evan Kramer
 # Set up
 import os
 os.chdir('U:/dc_public_salaries')
-from urllib.request import urlopen, Request 
+from urllib.request import urlopen, Request
 import requests
 from bs4 import BeautifulSoup
 import tabula
 import re
-import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-from datetime import datetime
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from datetime import datetime
 
 # Get html structure
 hdrs = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'}
@@ -26,16 +26,23 @@ for i in bs.findAll('a'):
     if '.pdf' in str(i) and 'employee' in str(i):
         # Extract url
         url = re.search('<a href="(.*?)"', str(i)).group(1)
-        filename = re.search('attachments/(.*?).*$', str(i)).group(1)
+        filename = re.search('attachments/(.*?)$', url).group(1)
         # Download file
-        print(url)
-        print(filename)
-        # temp = tabula.read_pdf(urlopen(Request(url, headers = hdrs)), pages = 'all')
-        # https://medium.com/better-programming/convert-tables-from-pdfs-to-pandas-with-python-d74f8ac31dc2
+        r = requests.get(url, headers = hdrs, stream = True)
+        # Write file to disk
+        try:
+            with open('U:/dc_public_salaries/Raw Salary Files/' + filename, 'wb') as f:
+                f.write(r.content)
+        except:
+            pass
     else:
         pass
-    
-temp = tabula.read_pdf('Raw Salary Files/public_body_employee_information_0314.pdf',
-                       pages = 'all')
 
-# Scrape/download salary files
+# Read from files
+os.chdir('U:/dc_public_salaries/Raw Salary Files/')
+for f in os.listdir('U:/dc_public_salaries/Raw Salary Files/'):
+    temp = tabula.read_pdf(f, pages = 'all').to_csv(f.replace('.pdf', '.csv'),
+                                                    index = False)
+        
+# https://medium.com/better-programming/convert-tables-from-pdfs-to-pandas-with-python-d74f8ac31dc2
+# https://stackabuse.com/download-files-with-python/
